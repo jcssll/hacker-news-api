8d420a803727e0eb929914e7728b2027ca7bc4ff
jcssll <essentialnetworker@gmail.com>
2025-04-15 22:27:38 -0400

    Create Story.cs
    Configured the story model

---
+++ HackerNewsApi/Models/Story.cs
@@ -0,0 +1,12 @@
+﻿namespace HackerNewsApi.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string By { get; set; }
+        public int Score { get; set; }
+        public long Time { get; set; }
+    }
+}
