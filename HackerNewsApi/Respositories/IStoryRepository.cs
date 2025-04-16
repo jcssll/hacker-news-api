bd10ed7de6fa59c43fd9e3067be24c4cb90e6699
jcssll <essentialnetworker@gmail.com>
2025-04-15 23:38:42 -0400

    Create IStoryRepository.cs
    add IStoryRepository interface for story data access
    
    - Defines method contracts for retrieving top stories and individual story by ID

---
+++ HackerNewsApi/Respositories/IStoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using HackerNewsApi.Models;
+
+namespace HackerNewsApi.Respositories
+{
+    public interface IStoryRepository
+    {
+        Task<IEnumerable<Story>> GetTopStoriesAsync();
+        Task<Story?> GetStoryByIdAsync(int id);
+    }
+}
