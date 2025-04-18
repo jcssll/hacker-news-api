2bf8a6b36724472a61bc75a93a86c920eaf30d9a
jcssll <essentialnetworker@gmail.com>
2025-04-18 14:22:42 -0400

    Update StoryRepository.cs
    Added more implementation methods as a solution to the error that Story Repository was not implementing methods in the Interface

--- HackerNewsApi/Respositories/StoryRepository.cs
+++ HackerNewsApi/Respositories/StoryRepository.cs
@@ -12,6 +12,7 @@ namespace HackerNewsApi.Respositories
         public StoryRepository(IHttpClientFactory httpClientFactory, IMemoryCache cache)
         {
             _httpClientFactory = httpClientFactory;
+            _cache = cache; 
         }
 
         public async Task<IEnumerable<Story>> GetNewestStoriesAsync(int page, int pageSize, string searchTerm)
@@ -35,6 +36,27 @@ namespace HackerNewsApi.Respositories
                 .Where(s => string.IsNullOrEmpty(searchTerm) || s.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
                 .Skip((page - 1) * pageSize)
                 .Take(pageSize);
+        }
+
+        public async Task<IEnumerable<Story>> GetTopStoriesAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var ids = await client.GetFromJsonAsync<List<int>>("https://hacker-news.firebaseio.com/v0/topstories.json");
+
+            var tasks = ids.Take(50).Select(id =>
+            client.GetFromJsonAsync<Story>($"https://hacker-news.firebaseio.com/v0/item/{id}.json")
+            );
+
+            var stories = (await Task.WhenAll(tasks))
+                .Where(s => s != null && !string.IsNullOrEmpty(s.Url)).ToList();
+
+            return stories; 
+        }
+
+        public async Task<Story?> GetStoryByIdAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            return await client.GetFromJsonAsync<Story>($"https://hacker-news.firebaseio.com/v0/item/{id}.json");
         }
 
     }
